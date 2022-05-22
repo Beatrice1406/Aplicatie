d3830f54140bc4d4fe38c51717a560f41f75ebfc
ioanacapra11 <102293162+ioanacapra11@users.noreply.github.com>
2022-05-22 10:33:57 +0300

    AI-34 Creates a new tab when the customer clicks on a house and it will appear details about the house

---
+++ src/main/java/org/loose/fis/sre/services/HouseService.java
@@ -0,0 +1,43 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.House;
+import org.loose.fis.sre.model.User;
+
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Objects;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+import static org.loose.fis.sre.services.FileSystemService.getPathToHouse;
+
+public class HouseService {
+
+    private static ObjectRepository<House> houseRepository;
+
+    public static void initDatabase() {
+        FileSystemService.initDirectory_house();
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToHouse("house-database.db").toFile())
+                .openOrCreate();
+        houseRepository = database.getRepository(House.class);
+    }
+    public static ObjectRepository<House> getHouseRepository()
+    {
+        return houseRepository;
+    }
+
+    public static String  seeHouses()
+    {
+        String s="";
+        for (House house : houseRepository.find())
+        {
+
+            s = s + house.toString();
+            s = s + "\n";
+        }
+        return s;
+    }
+}
